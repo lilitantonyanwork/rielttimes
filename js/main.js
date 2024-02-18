e60da97eca2cadb12a54e00213e741182b707c68
lilit <lilit@helix.am>
2024-02-18 12:08:55 +0400

    responsive style

--- js/main.js
+++ js/main.js
@@ -8,25 +8,36 @@ $(function (){
         centerMode: true,
     });
 
-    $('.features__slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-    });
-    $('.afisha__list').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        arrows: false,
-        dots: true,
-    });
-    $('.subject__list').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        arrows: false,
-        dots: true,
-    });
+
+    $(window).on('resize', function (){
+        if($(window).with()<991){
+            $('.features__slider').slick('unslick');
+            $('.afisha__list').slick('unslick');
+            $('.subject__list').slick('unslick');
+
+        } else{
+            $('.features__slider').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+            });
+            $('.afisha__list').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                arrows: false,
+                dots: true,
+            });
+            $('.subject__list').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                arrows: false,
+                dots: true,
+            });
+
+        }
+    })
 })
